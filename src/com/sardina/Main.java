ea7ac70749dc39324f702208d441c5eca61515e4
Christopher Sardina <chrissardina@Chris-Sardina.local>
2017-09-13 18:00:48 -0400

    Java - Math Ops initial commit.

---
+++ src/com/sardina/Main.java
@@ -0,0 +1,12 @@
+package com.sardina;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please input 2 numbers.");
+    }
+}
