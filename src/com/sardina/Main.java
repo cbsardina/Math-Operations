311968e9edb11ccf2777380a7471b74846564ec7
Christopher Sardina <chrissardina@Chris-Sardina.local>
2017-09-13 18:40:06 -0400

    Sum, diff, division, multiplication completed and debugged.

--- src/com/sardina/Main.java
+++ src/com/sardina/Main.java
@@ -7,6 +7,22 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Please input 2 numbers.");
+        System.out.println("Please enter a number.");
+        String number1 = scanner.nextLine();
+
+        System.out.println("Please enter a second number.");
+        String number2 = scanner.nextLine();
+
+        double operand1 = Double.parseDouble(number1);
+        double operand2 = Double.parseDouble(number2);
+
+        double sum = operand1 + operand2;
+        double difference = operand1 - operand2;
+        double division = operand1 / operand2;
+        double multiplication = operand1 * operand2;
+
+
+        System.out.println("Just text");
     }
+
 }
